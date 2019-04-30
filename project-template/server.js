91786f88c1551422841a1413f2bfe56888e67943
tahseenrhn <tahseenrahma@umass.edu>
2019-04-30 13:31:31 -0400

    Updated MongoDB

--- project-template/server.js
+++ project-template/server.js
@@ -8,6 +8,53 @@ app.use(bodyParser.json());
 
 const MongoClient = require('mongodb').MongoClient;
 
+const requestFieldType = {
+
+    id: 'optional',
+    name: 'required',
+
+};
+
+function validateRequest(name) {
+  const errors = [];
+  Object.keys(requestFieldType).forEach(field => {
+    if (requestFieldType[field] === 'required' && !name[field]) {
+      errors.push(`Missing mandatory field: ${field}`);
+    }
+  });
+  return (errors.length ? errors.join('; ') : null);
+}
+
+function cleanupRequest(name) {
+  const cleanedUpRequest = {};
+  Object.keys(name).forEach(field => {
+    if (requestFieldType[field]) cleanedUpRequest[field] = name[field];
+  });
+  return cleanedUpRequest;
+}
+
+app.get('/api/requests/:id', (req, res) => {
+  let requestId;
+  try {
+    requestId = new ObjectId(req.params.id);
+  }
+  catch (error) {
+    res.status(422).json({ message: `Invalid request ID format: ${error}` });
+    return;
+  }
+
+  db.collection('ez-lease').find({ _id: requestId }).limit(1)
+  .next()
+  .then(request => {
+    if (!request) res.status(404).json({ message: `No such request: ${requestId}` });
+    else res.json(request);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({ message: `Internal Server Error: ${error}` });
+  });
+});
+
 let db;
 MongoClient.connect('mongodb://localhost', { useNewUrlParser: true }).then(connection => {
   db = connection.db('ez-lease');
